1567bc132366e9b39387c7db607be6d2b707c732
𝗞𝗔𝗩𝗜-𝗘𝗫𝗘 𝗢𝗪𝗡𝗘𝗥 👨‍💻💗 <tharushankavishka02@gmail.com>
2025-03-01 13:34:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=ccwVVYoB#cbdMg-fbCTew9x8jYJiNUc0uNq92za77-ZsOTrMZS-g",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=sRRR2QJZ#hmY6ESyH6UVUrTYhjf7mUNbZvI6IE5iMbhJ5Ag968O0",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
 MODE: process.env.MODE || "public",
